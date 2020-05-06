a123faf51d0603c23fab355da97c60e4554707b3
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-05-06 15:49:59 +0900

    과제 문제 4번

--- onecondition/Program.cs
+++ onecondition/Program.cs
@@ -10,6 +10,15 @@ namespace onecondition
     {
         static void Main(string[] args)
         {
+            int x, y;
+            Console.WriteLine("--숫자를 입력해--");
+            Console.Write("x: ");
+            x = int.Parse(Console.ReadLine());//입력~
+            
+            if (x>10 && x < 20)
+            {
+                Console.WriteLine("조건에 맞습니다.");
+            }
         }
     }
 }
